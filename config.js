82f5bd6dbcca2a169d9e221eeef8fe8b800a664e
onakamaruido-source <onaka.marui.do@gmail.com>
2026-08-02 20:10:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbwAUzHMq-2wXodlWIfXtGVPXaYgYbhR9PgU9tO2d5a4GF4jbx2ILtGnJX-asXHcR-xU/exec";
+"https://script.google.com/macros/s/AKfycbwtKrT8_x9FJN25hr3T2xtdDmEttl2zImVvDWXkgJo0VMPcO178N9ALRDxcaUG37FlA0w/exec";
